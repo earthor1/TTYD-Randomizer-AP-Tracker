edf99640dd4ed9436390f2895d5456c6a58499ba
earthor1 <earthor1@gmail.com>
2025-05-04 00:24:00 -0400

    Code cleanup
    cleaning up code a bit:
    
    - removing comments of functions already called out
    - removing code from archipelago.lua that's already called from utils.lua
    - deprecating settings.lua since the configuration can just be included in autotracking.lua

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -1,39 +1,9 @@
 
-        ScriptHost:LoadScript("scripts/autotracking/item_mapping.lua")
+ScriptHost:LoadScript("scripts/autotracking/item_mapping.lua")
 ScriptHost:LoadScript("scripts/autotracking/location_mapping.lua")
 
 CUR_INDEX = -1
---SLOT_DATA = nil
-
 SLOT_DATA = {}
-
-function has_value (t, val)
-    for i, v in ipairs(t) do
-        if v == val then return 1 end
-    end
-    return 0
-end
-
-function dump_table(o, depth)
-    if depth == nil then
-        depth = 0
-    end
-    if type(o) == 'table' then
-        local tabs = ('	'):rep(depth)
-        local tabs2 = ('	'):rep(depth + 1)
-        local s = '{'
-        for k, v in pairs(o) do
-            if type(k) ~= 'number' then
-                k = '"' .. k .. '"'
-            end
-            s = s .. tabs2 .. '[' .. k .. '] = ' .. dump_table(v, depth + 1) .. ','
-        end
-        return s .. tabs .. '}'
-    else
-        return tostring(o)
-    end
-end
-
 
 function onClear(slot_data)
     --SLOT_DATA = slot_data
@@ -59,25 +29,7 @@ function onClear(slot_data)
           item_code = item[1]
           item_type = item[2]
           initial_state = item[3]
---            if item_code and item[2] then
             local item_obj = Tracker:FindObjectForCode(item_code)
---            if item_obj then
---                if item_type == "toggle" then
---                    item_obj.Active = false
---                elseif item_type == "progressive" then
---                    item_obj.CurrentStage = 0
---                    item_obj.Active = false
---                elseif item_type == "consumable" then
---                    if item_obj.MinCount then
---                        item_obj.AcquiredCount = item_obj.MinCount
---                    else
---                        item_obj.AcquiredCount = 0
---                    end
---                elseif item_type == "progressive_toggle" then
---                    item_obj.CurrentStage = 0
---                    item_obj.Active = false
---                end
---            end
             if item_obj then
                 if item_obj.Type == "toggle" then
                     item_obj.Active = false
@@ -96,14 +48,10 @@ function onClear(slot_data)
                 end
             end
         end
---        end
     end
     PLAYER_ID = Archipelago.PlayerNumber or -1
     TEAM_NUMBER = Archipelago.TeamNumber or 0
     SLOT_DATA = slot_data
-    -- if Tracker:FindObjectForCode("autofill_settings").Active == true then
-    --     autoFill(slot_data)
-    -- end
 end
 
 function onItem(index, item_id, item_name, player_number)
@@ -117,32 +65,10 @@ function onItem(index, item_id, item_name, player_number)
         --print(string.format("onItem: could not find item mapping for id %s", item_id))
         return
     end
---    for _, item_code in pairs(item[1]) do
-        -- print(item[1], item[2])
+
     item_code = item[1]
     item_type = item[2]
     local item_obj = Tracker:FindObjectForCode(item_code)
---    if item_obj then
---        if item_type == "toggle" then
---            -- print("toggle")
---            item_obj.Active = true
---        elseif item_type == "progressive" then
---            -- print("progressive")
---            item_obj.Active = true
---        elseif item_type == "consumable" then
---            -- print("consumable")
---            item_obj.AcquiredCount = item_obj.AcquiredCount + item_obj.Increment
---        elseif item_type == "progressive_toggle" then
---            -- print("progressive_toggle")
---            if item_obj.Active then
---                item_obj.CurrentStage = item_obj.CurrentStage + 1
---            else
---                item_obj.Active = true
---            end
---        end
---    else
---        print(string.format("onItem: could not find object for code %s", item_code[1]))
---    end
     if item_obj then
         if item_obj.Type == "toggle" then
             -- print("toggle")
@@ -164,7 +90,6 @@ function onItem(index, item_id, item_name, player_number)
     else
         print(string.format("onItem: could not find object for code %s", item_code[1]))
     end
---    end
 end
 
 --called when a location gets cleared
@@ -197,38 +122,6 @@ end
 function onEventsLaunch(key, value)
     updateEvents(value)
 end
-
--- function autoFill()
---     if SLOT_DATA == nil  then
---         print("its fucked")
---         return
---     end
---     -- print(dump_table(SLOT_DATA))
-
---     mapToggle={[0]=0,[1]=1,[2]=1,[3]=1,[4]=1}
---     mapToggleReverse={[0]=1,[1]=0,[2]=0,[3]=0,[4]=0}
---     mapTripleReverse={[0]=2,[1]=1,[2]=0}
-
---     slotCodes = {
---         map_name = {code="", mapping=mapToggle...}
---     }
---     -- print(dump_table(SLOT_DATA))
---     -- print(Tracker:FindObjectForCode("autofill_settings").Active)
---     if Tracker:FindObjectForCode("autofill_settings").Active == true then
---         for settings_name , settings_value in pairs(SLOT_DATA) do
---             -- print(k, v)
---             if slotCodes[settings_name] then
---                 item = Tracker:FindObjectForCode(slotCodes[settings_name].code)
---                 if item.Type == "toggle" then
---                     item.Active = slotCodes[settings_name].mapping[settings_value]
---                 else 
---                     -- print(k,v,Tracker:FindObjectForCode(slotCodes[k].code).CurrentStage, slotCodes[k].mapping[v])
---                     item.CurrentStage = slotCodes[settings_name].mapping[settings_value]
---                 end
---             end
---         end
---     end
--- end
 
 
 -- ScriptHost:AddWatchForCode("settings autofill handler", "autofill_settings", autoFill)
