e34cc76e53f6b8d5fe4dbac76ffd7717f53c059c
PillarAngel <gregorymg@rogers.com>
2025-05-09 15:47:06 -0400

    Added tested and working code for auto tracking.

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -1,5 +1,6 @@
 ScriptHost:LoadScript("scripts/autotracking/item_mapping.lua")
 ScriptHost:LoadScript("scripts/autotracking/location_mapping.lua")
+ScriptHost:LoadScript("scripts/autotracking/map_mapping.lua")
 
 CUR_INDEX = -1
 SLOT_DATA = {}
@@ -9,63 +10,57 @@ function onClear(slot_data)
     TEAM_NUMBER = Archipelago.TeamNumber or 0
     SLOT_DATA = slot_data
     CUR_INDEX = -1
-    -- reset locations
-    for _, location_array in pairs(LOCATION_MAPPING) do
-        for _, location in pairs(location_array) do
-            if location then
-                local location_obj = Tracker:FindObjectForCode(location)
-                if location_obj then
-                    if location:sub(1, 1) == "@" then
-                        location_obj.AvailableChestCount = location_obj.ChestCount
-                    else
-                        location_obj.Active = false
-                    end
-                end
-            end
-        end
+
+    -- Map Datastorage
+    if Archipelago.PlayerNumber > -1 then
+        print("SUCCESS?")
+        cur_room = "ttyd_room_" .. TEAM_NUMBER .. "_" .. PLAYER_ID
+        Archipelago:SetNotify({cur_room})
+        Archipelago:Get({cur_room})
     end
-    -- reset items
-    for _, item in pairs(ITEM_MAPPING) do
-        for _, item_code in pairs(item) do
-          item_code = item[1]
-          item_type = item[2]
-          initial_state = item[3]
-            local item_obj = Tracker:FindObjectForCode(item_code)
-            if item_obj then
-                if item_obj.Type == "toggle" then
-                    item_obj.Active = false
-                elseif item_obj.Type == "progressive" then
-                    item_obj.CurrentStage = 0
-                    item_obj.Active = initial_state or false
-                elseif item_obj.Type == "consumable" then
-                    if item_obj.MinCount then
-                        item_obj.AcquiredCount = item_obj.MinCount
-                    else
-                        item_obj.AcquiredCount = 0
-                    end
-                elseif item_obj.Type == "progressive_toggle" then
-                    item_obj.CurrentStage = 0
-                    item_obj.Active = initial_state or false
+end
+
+-- reset locations
+for _, location_array in pairs(LOCATION_MAPPING) do
+    for _, location in pairs(location_array) do
+        if location then
+            local location_obj = Tracker:FindObjectForCode(location)
+            if location_obj then
+                if location:sub(1, 1) == "@" then
+                    location_obj.AvailableChestCount = location_obj.ChestCount
+                else
+                    location_obj.Active = false
                 end
             end
         end
     end
 end
-
--- Placeholder code for auto tab switching.
---function onNotify(key, value, old_value)
---    if value ~= old_value then
---        if key == cur_stage then
---            print("map"..value)
---        end
---    end
---end
-
---function onNotifyLaunch(key, value)
---    if key == cur_stage then
---        print("map: "..value)
---    end
---end
+-- reset items
+for _, item in pairs(ITEM_MAPPING) do
+    for _, item_code in pairs(item) do
+        item_code = item[1]
+        item_type = item[2]
+        initial_state = item[3]
+        local item_obj = Tracker:FindObjectForCode(item_code)
+        if item_obj then
+            if item_obj.Type == "toggle" then
+                item_obj.Active = false
+            elseif item_obj.Type == "progressive" then
+                item_obj.CurrentStage = 0
+                item_obj.Active = initial_state or false
+            elseif item_obj.Type == "consumable" then
+                if item_obj.MinCount then
+                    item_obj.AcquiredCount = item_obj.MinCount
+                else
+                    item_obj.AcquiredCount = 0
+                end
+            elseif item_obj.Type == "progressive_toggle" then
+                item_obj.CurrentStage = 0
+                item_obj.Active = initial_state or false
+            end
+        end
+    end
+end
 
 function onItem(index, item_id, item_name, player_number)
     if index <= CUR_INDEX then
@@ -128,11 +123,37 @@ function onLocation(location_id, location_name)
     end
 end
 
+-- Code for auto tab switching.
+function onNotify(key, value, old_value)
+    if value ~= old_value then
+        if key == cur_room then
+            print("yeah")
+            print("map: " .. value)
+        end
+    end
+end
+
+function onNotifyLaunch(key, value)
+    if key == cur_room then
+        print("yeah")
+        print("map: " .. value)
+    end
+end
+
+function onMapChange(key, value, old)
+    tabs = MAP_MAPPING[tostring(value)]
+    for i, tab in ipairs(tabs) do
+        Tracker:UiHint("ActivateTab", tab)
+    end
+end
+
 -- ScriptHost:AddWatchForCode("settings autofill handler", "autofill_settings", autoFill)
 Archipelago:AddClearHandler("clear handler", onClear)
 Archipelago:AddItemHandler("item handler", onItem)
 Archipelago:AddLocationHandler("location handler", onLocation)
 
--- Placeholder code for auto tab switching.
---Archipelago:AddSetReplyHandler("notify handler", onNotify)
---Archipelago:AddRetrievedHandler("notify launch handler", onNotifyLaunch)
+-- Code for auto tab switching.
+Archipelago:AddSetReplyHandler("notify handler", onNotify)
+Archipelago:AddRetrievedHandler("notify launch handler", onNotifyLaunch)
+Archipelago:AddSetReplyHandler("map_key", onMapChange)
+Archipelago:AddRetrievedHandler("map_key", onMapChange)
