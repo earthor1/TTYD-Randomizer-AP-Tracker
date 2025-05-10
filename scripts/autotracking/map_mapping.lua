a1d3c2f7ab1cf758f3eebfa78927092f96b5a0c6
PillarAngel <gregorymg@rogers.com>
2025-05-10 03:52:34 -0400

    Populated Table with all map IDs in the game. Tab mappings subject to change.

--- scripts/autotracking/map_mapping.lua
+++ scripts/autotracking/map_mapping.lua
@@ -1,4 +1,101 @@
-MAP_MAPPING = {
-    ["gor_02"] = {"Rogueport", "Rogueport"},
-    ["tik_01"] = {"Rogueport", "Underground"}
-}
+MAP_MAPPING = {}
+
+--Rogueport
+for i = 0, 4 do
+    MAP_MAPPING[string.format("gor_%02d", i)] = {"Rogueport", "Rogueport"}
+end
+
+--Underground
+for i = 0, 21 do
+    MAP_MAPPING[string.format("tik_%02d", i)] = {"Rogueport", "Underground"}
+end
+
+--Hooktail Castle
+for i = 0, 13 do
+    MAP_MAPPING[string.format("gon_%02d", i)] = {"Chapter 1"}
+end
+
+--Petal Meadows
+for i = 0, 13 do
+    MAP_MAPPING[string.format("hei_%02d", i)] = {"Chapter 1"}
+end
+
+--Petalburg
+for i = 0, 1 do
+    MAP_MAPPING[string.format("nok_%02d", i)] = {"Chapter 1"}
+end
+
+--Great Tree
+for i = 0, 20 do
+    MAP_MAPPING[string.format("mri_%02d", i)] = {"Chapter 2"}
+end
+
+--Boggly Woods
+for i = 0, 6 do
+    MAP_MAPPING[string.format("win_%02d", i)] = {"Chapter 2"}
+end
+
+--Glitzville
+for i = 0, 13 do
+    MAP_MAPPING[string.format("tou_%02d", i)] = {"Chapter 3"}
+end
+
+--Twilight Trail
+for i = 0, 6 do
+    MAP_MAPPING[string.format("gra_%02d", i)] = {"Chapter "}
+end
+
+--Creepy Steeple
+for i = 0, 11 do
+    MAP_MAPPING[string.format("jin_%02d", i)] = {"Chapter 4"}
+end
+
+--Twilight Town
+for i = 0, 1 do
+    MAP_MAPPING[string.format("usu_%02d", i)] = {"Chapter 4"}
+end
+
+--Pirate's Grotto
+for i = 0, 13 do
+    MAP_MAPPING[string.format("dou_%02d", i)] = {"Chapter 5"}
+end
+
+--Keelhaul Key
+for i = 0, 12 do
+    MAP_MAPPING[string.format("muj_%02d", i)] = {"Chapter 5"}
+end
+
+--Poshley Heights
+for i = 0, 4 do
+    MAP_MAPPING[string.format("pik_%02d", i)] = {"Chapter 6"}
+end
+
+--Excess Express
+for i = 0, 6 do
+    MAP_MAPPING[string.format("rsh_%02d", i)] = {"Chapter 6"}
+end
+
+--X-Naut Fortress
+for i = 0, 19 do
+    MAP_MAPPING[string.format("aji_%02d", i)] = {"Chapter 7"}
+end
+
+--Fahr Outpost
+for i = 0, 4 do
+    MAP_MAPPING[string.format("bom_%02d", i)] = {"Chapter 7"}
+end
+
+--The Moon
+for i = 0, 7 do
+    MAP_MAPPING[string.format("moo_%02d", i)] = {"Chapter 7"}
+end
+
+--The Thousand-Year Door
+for i = 0, 30 do
+    MAP_MAPPING[string.format("las_%02d", i)] = {"Chapter 8"}
+end
+
+--Pit of 100 Trials
+for i = 0, 6 do
+    MAP_MAPPING[string.format("jon_%02d", i)] = {"Magical Map"}
+end
