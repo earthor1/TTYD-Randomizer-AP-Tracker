830b23a4079ebcb9de606d3978b1335e85e0d7f1
PillarAngel <gregorymg@rogers.com>
2025-06-01 18:54:27 -0400

    Removed redundant Autotab code.

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -143,24 +143,6 @@ function onLocation(location_id, location_name)
 end
 
 -- Code for auto tab switching.
-function onNotify(key, value, old_value)
-    if has("AutoTabOn") then
-        if value ~= old_value then
-            if key == cur_room then
-                print("map: " .. value)
-            end
-        end
-    end
-end
-
-function onNotifyLaunch(key, value)
-    if has("AutoTabOn") then
-        if key == cur_room then
-            print("map: " .. value)
-        end
-    end
-end
-
 function onMapChange(key, value, old)
     if has("AutoTabOn") then
         tabs = MAP_MAPPING[tostring(value)]
@@ -169,13 +151,12 @@ function onMapChange(key, value, old)
         end
     end
 end
+
 -- ScriptHost:AddWatchForCode("settings autofill handler", "autofill_settings", autoFill)
 Archipelago:AddClearHandler("clear handler", onClear)
 Archipelago:AddItemHandler("item handler", onItem)
 Archipelago:AddLocationHandler("location handler", onLocation)
 
 -- Code for auto tab switching.
-Archipelago:AddSetReplyHandler("notify handler", onNotify)
-Archipelago:AddRetrievedHandler("notify launch handler", onNotifyLaunch)
 Archipelago:AddSetReplyHandler("map_key", onMapChange)
 Archipelago:AddRetrievedHandler("map_key", onMapChange)
