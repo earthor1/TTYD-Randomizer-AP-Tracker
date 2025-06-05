248b3ee1ef855c98064511a46793848bc73d5a09
PillarAngel <gregorymg@rogers.com>
2025-06-04 21:08:44 -0400

    Added full tracking to Hooktail castle, and all Chapter 2 and 3 maps. (earthor1)

--- scripts/locations_import.lua
+++ scripts/locations_import.lua
@@ -2,5 +2,11 @@ Tracker:AddLocations("locations/Overworld.json")
 Tracker:AddLocations("locations/Town.json")
 Tracker:AddLocations("locations/Sewers.json")
 Tracker:AddLocations("locations/Petal_Meadows.json")
-Tracker:AddLocations("locations/Shhwonk_Fortress.json")
+Tracker:AddLocations("locations/Hooktail_Castle.json")
+Tracker:AddLocations("locations/Boggly_Woods.json")
+Tracker:AddLocations("locations/Great_Tree.json")
+Tracker:AddLocations("locations/Glitz_Pit.json")
+Tracker:AddLocations("locations/Glitzville.json")
+
+--Player Position Tracking
 Tracker:AddLocations("locations/Player_Positions.json")
