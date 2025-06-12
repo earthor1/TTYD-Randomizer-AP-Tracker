751d4705e42397b835f8027cd0f74375fad80767
earthor1 <earthor1@gmail.com>
2025-06-12 16:09:25 -0400

    HRG implementation + bug fixes

--- scripts/logic/logic.lua
+++ scripts/logic/logic.lua
@@ -35,7 +35,7 @@ function pit()
 	end
 
 function sewerwest()
-	return (has("ContactLens") and has("PaperCurse")) or (has("UltraHammer") and has("PaperCurse")) or (tube()) or (has("Bobbery"))
+	return (has("ContactLens") and has("PaperCurse")) or (has("UltraHammer") and ((has("PaperCurse")) or has("UltraBoots"))) or (tube()) or (has("Bobbery"))
 	end
 
 function sewerwestground()
