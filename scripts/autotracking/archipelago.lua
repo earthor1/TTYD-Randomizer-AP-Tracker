e68caa56a1cc0268ec83a9b5a767f2732c1719ca
PillarAngel <gregorymg@rogers.com>
2025-06-18 22:17:36 -0400

    Removed hint clear function.

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -34,7 +34,6 @@ function onClear(slot_data)
                     else
                         location_obj.Active = false
                     end
-                    updateHintsClear(location)
                 end
             end
         end
@@ -220,25 +219,6 @@ function updateHints(locationID, status)
             print(string.format("No object found for code: %s", location))
         end
     end
-end
-
-function updateHintsClear(locationID)
-	if not Highlight then
-		return
-	end
-
-	if not LOCATION_MAPPING[locationID] then
-		return
-	end
-
-	local location_name = LOCATION_MAPPING[locationID][1][1]
-	local obj = Tracker:FindObjectForCode(location_name)
-
-	if obj then
-		obj.Highlight = Highlight.None
-	else
-		print(string.format("No object found for code: %s", location_name))
-	end
 end
 
 -- ScriptHost:AddWatchForCode("settings autofill handler", "autofill_settings", autoFill)
